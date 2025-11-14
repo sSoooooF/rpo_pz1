13caa20ea6a33e5bca7e79bb5dd93d6f486cbef9
Nail <nurievnail03@mail.ru>
2025-11-14 17:36:01 +0300

    cherry-pick into dev1

--- app1/bin/app1.dart
+++ app1/bin/app1.dart
@@ -4,4 +4,8 @@ import 'package:app1/app1.dart' as app1;
 void main(List<String> arguments) {
   print('Hello world: ${app1.calculate()}!');
 }
+<<<<<<< HEAD
 "// person1 conflict" 
+=======
+"// make conflict from person2" 
+>>>>>>> 21798fe (conflict change in app1.dart by person2 in dev2)
